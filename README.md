3c2fa71c1b3fc3f69a3382906d30c8eba3439016
Mark <markjsapp@gmail.com>
2024-01-31 23:04:48 -0500

    update the README, add additional logic for allure metadata

--- README.md
+++ README.md
@@ -37,7 +37,6 @@ It is assumed that you have the following dependencies installed in your project
     @wdio/allure-reporter
     @wdio/globals
     @wdio/junit-reporter
-    chromedriver
 
 ## 🔌 Installation 🔌
  
@@ -67,8 +66,49 @@ describe('My Feature Tests', function() {
 
 This probably goes without saying but you'll also need to update your WDIO config file and change the reporter to Allure.
 
+## 🧠 Supported Allure API Commands 🧠
+@wdio/allure-reporter documentation can be found ![here](https://webdriver.io/docs/allure-reporter/#supported-allure-api).
+
+* ```addLabel(name, value)``` - assign a custom label to test
+
+* ```addFeature(featureName)``` – assign features to test
+
+* ```addStory(storyName)``` – assign user story to test
+
+* ```addSeverity(value)``` – assign severity to test, accepts one of these values: blocker, - critical, normal, minor, trivial
+
+* ```addTag(value)``` – assign a tag label to test
+
+* ```addEpic(value)``` – assign an epic label to test
+
+* ```addOwner(value)``` – assign an owner label to test
+
+* ```addSuite(value)``` – assign a suite label to test
+
+* ```addSubSuite(value)``` – assign a sub suite label to test
+
+* ```addParentSuite(value)``` – assign a parent suite label to test
+
+* ```addIssue(value)``` – assign issue id to test
+
+* ```addAllureId(value)``` – assign allure test ops id label to test
+
+* ```addTestId(value)``` – assign TMS test id to test
+
+* ```addArgument(name, value)``` - add an additional argument to test
+
+* ```addDescription(description, [type])``` – add description to test
+
+    - ```description (String)``` - description of the test
+
+    - ```type (String, optional)``` – description type, text by 
+    default. Values ['text', 'html','markdown']
+
 ## 🎸 Report Example 🎸
 ![Report](https://i.imgur.com/vVyXsfr.jpg)
 
-## License
+## 🗯️ Troubleshooting 🗯️
+You may need to ![download](https://www.oracle.com/java/technologies/downloads/) the latest Java SDK if you have problems launching allure.
+
+## 💫 License 💫
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,21 @@ import Mocha from 'mocha';
 import allureReporter from '@wdio/allure-reporter';
 
 interface AllureConfig {
-  addTag?: string;
-  addEpic?: string;
-  addStory?: string;
-  addDescription?: { description: string; type?: string };
-  jiraTicketId?: string;
+    addTag?: string | string[];
+    addFeature?: string;
+    addStory?: string;
+    addSeverity?: 'blocker' | 'critical' | 'normal' | 'minor' | 'trivial';
+    addEpic?: string;
+    addOwner?: string;
+    addSuite?: string;
+    addSubSuite?: string;
+    addParentSuite?: string;
+    addIssue?: string;
+    addAllureId?: string;
+    addTestId?: string;
+    addArgument?: { name: string; value: string }[];
+    addDescription?: { description: string; type?: string };
+    jiraTicketId?: string;
 }
 
 // Define a wrapper function for `it` that includes the `.meta` functionality
@@ -24,23 +34,58 @@ itWithMeta.meta = (allureConfig?: AllureConfig) => (title: string, fn: (this: Mo
 };
 
 function applyAllureConfig(allureConfig: AllureConfig) {
-  if (allureConfig.addTag) {
-    allureReporter.addLabel('tag', allureConfig.addTag);
-  }
-  if (allureConfig.addEpic) {
-    allureReporter.addLabel('epic', allureConfig.addEpic);
-  }
-  if (allureConfig.addStory) {
-    allureReporter.addLabel('story', allureConfig.addStory);
-  }
-  if (allureConfig.addDescription) {
-    allureReporter.addDescription(allureConfig.addDescription.description, allureConfig.addDescription.type || 'text');
-  }
-  if (allureConfig.jiraTicketId) {
-    const jiraBaseUrl = 'https://example.atlassian.net/browse/';
-    const fullJiraLink = jiraBaseUrl + allureConfig.jiraTicketId;
-    allureReporter.addLink( fullJiraLink,'Jira Ticket', 'issue');
-  }
+    if (allureConfig.addTag) {
+        const tags = Array.isArray(allureConfig.addTag) ? allureConfig.addTag : [allureConfig.addTag];
+        tags.forEach(tag => {
+          allureReporter.addLabel('tag', tag);
+        });
+      }
+    if (allureConfig.addFeature) {
+      allureReporter.addFeature(allureConfig.addFeature);
+    }
+    if (allureConfig.addStory) {
+      allureReporter.addStory(allureConfig.addStory);
+    }
+    if (allureConfig.addSeverity) {
+      allureReporter.addSeverity(allureConfig.addSeverity);
+    }
+    if (allureConfig.addEpic) {
+      allureReporter.addEpic(allureConfig.addEpic);
+    }
+    if (allureConfig.addOwner) {
+      allureReporter.addOwner(allureConfig.addOwner);
+    }
+    if (allureConfig.addSuite) {
+      allureReporter.addSuite(allureConfig.addSuite);
+    }
+    if (allureConfig.addSubSuite) {
+      allureReporter.addSubSuite(allureConfig.addSubSuite);
+    }
+    if (allureConfig.addParentSuite) {
+      allureReporter.addParentSuite(allureConfig.addParentSuite);
+    }
+    if (allureConfig.addIssue) {
+      allureReporter.addIssue(allureConfig.addIssue);
+    }
+    if (allureConfig.addAllureId) {
+      allureReporter.addAllureId(allureConfig.addAllureId);
+    }
+    if (allureConfig.addTestId) {
+      allureReporter.addTestId(allureConfig.addTestId);
+    }
+    if (allureConfig.addArgument) {
+      allureConfig.addArgument.forEach(argument => {
+        allureReporter.addArgument(argument.name, argument.value);
+      });
+    }
+    if (allureConfig.addDescription) {
+      allureReporter.addDescription(allureConfig.addDescription.description, allureConfig.addDescription.type || 'text');
+    }
+    if (allureConfig.jiraTicketId) {
+      const jiraBaseUrl = 'https://example.atlassian.net/browse/';
+      const fullJiraLink = jiraBaseUrl + allureConfig.jiraTicketId;
+      allureReporter.addLink(fullJiraLink, 'Jira Ticket', 'issue');
+    }
 }
 
 // Export the wrapper function
